561e6582f80fb6b2109acc901f474e58b134c503
pste <pirraste@gmail.com>
2014-02-26 22:14:53 +0100

    rewritten layout

--- README.md
+++ README.md
@@ -1,3 +1,81 @@
 jap - just another player
 
-Jap (as in "Japan") is a jQueryUI styled HTML5 audio player.
+Jap (as in "Japan") is a jQueryUI styled HTML5 audio player. It is freely usable, but
+still in development (don't consider this a stable / final version).
+
+TODO
+- Playlist buttons (shuffle, sort -with custom fn-, add -browse local/prompt uri-, remove)
+- highlight "now playing" in playlist
+
+KNOWN BUGS
+none
+
+WORKS ON
+Everything handles jQuery and HTML5 stuff (canvas, audio).
+
+
+BASIC USAGE
+Given a "<div id='jap'></div>" it can be build with:
+$("#jap").jap();
+The '#jap' div became the container for all the html stuff needed by the player.
+
+
+ADVANCED USAGE
+It can be used as a wrapper for a custom player web page.
+See this example:
+$("#jap").jap().hide();
+Then somewhere in your code:
+$("#jap").jap("enqueue", {src: "http://127.0.0.1/Get?num=1"});
+$("#jap").jap("enqueue", {src: "http://127.0.0.1/Get?num=2"});
+$("#jap").jap("enqueue", {src: "http://127.0.0.1/Get?num=3"});
+$("#jap").jap("play");
+
+
+PARAMETERS
+All the default parameters are exposed here:
+$.fn.jap.defaults
+They are:
+- buttonSize (default: 30): the size of the jap controls
+- volume (default: 20): the initial value of the volume. It is a percentage [0:100] value.
+- formatTitle (default: function(src, metadata) { return src; }): it is the formatting function for the song title. It is
+used by the playlist and the "now playing" scrolling view.
+
+These can be customized in the jap constructor:
+$("#jap").jap({buttonSize: 50});
+$("#jap").jap({
+				formatTitle: function(src ,metadata) {	
+					if ($.isEmptyObject(metadata)) return src;
+					return metadata.author + " - " + metadata.album + " - " + metadata.trackn + " - " + metadata.title;
+				}
+			});
+
+
+EVENTS (API)
+- enqueue:
+$("#controls").jap("enqueue", 	{ src: "/home/my/music/is/here/track1.mp3", metadata: {title: "The wonderful song", album: "A new Album"} });
+Src (required): the uri of the song.
+Metadata (optional): it is a custom JSON object. It is passed to the "formatTitle" function so you can customize the way jap displays the song title.
+
+- dequeue
+$("#controls").jap("dequeue", 	{ index: 3 });
+Index (required): the playlist index of the item to remove.
+
+- seek
+$("#controls").jap("seek", 		{ percentage: 20 });
+Percentage (required): a [0:100] value.
+
+- play
+$("#controls").jap("play");
+
+- pause
+$("#controls").jap("pause");
+
+- stop
+$("#controls").jap("stop");
+
+- next
+$("#controls").jap("next");
+
+- prev
+$("#controls").jap("prev");
+
--- jap.jquery.js
+++ jap.jquery.js
@@ -2,11 +2,16 @@
 // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
 // http://www.w3schools.com/tags/ref_av_dom.asp
 (function($) {
+	function dbg(fmt, p1, p2, p3, p4, p5) {
+		if (console) console.log(fmt, p1, p2, p3, p4, p5);
+	}
+	
 	/* ::::::::::::::::::: */
 	
 	var settings = null;
 	var controls = null; // save controls for convenience
 	var animationOn = false;
+	var fontColor = "#00FFDE";
 	
 	var css = {
 		active: "ui-state-error"
@@ -22,27 +27,28 @@
 		// extend settings
 		settings = $.extend( {}, $.fn.jap.defaults, options );
 		settings.volume = Math.min(settings.volume, 100);
-	
+		var fontSize = settings.buttonSize/3;
+		var button_1 = settings.buttonSize;
+		var button_2 = settings.buttonSize/2;
+		var button_3 = settings.buttonSize/3;
+		var miniButtonSize = button_2 - 2; // FIXED VAL (2 border)
+		
 		// build interface
-		var miniButtonSize = (settings.buttonSize/2) - 2; // FIXED VAL (2)
-		var block1 = $("<div style='display: inline; float: left'></div>");
-		var block2 = $("<div style='display: inline; float: right'></div>");
-		var block3 = $("<div style='padding: 1px'></div>").css({"width": settings.buttonSize, "height": settings.buttonSize, "float": "left"}); // FIXED VAL (1)
-		container.css({"font-size": 9, "padding": 5}).addClass("ui-widget ui-widget-content ui-corner-all");
 		controls = {
-			audio:		$("<audio></audio>")			
-			, queue:	$("<ol class='queue ui-widget ui-widget-content ui-corner-all'></ol>").selectable().hide().css({"position":"absolute", "list-style-type":"none", "margin": 0, "padding": 7, "font-size": "1.1em", "overflow-y":"scroll", "max-height":"200px"})
-			, prev: 	$('<div class="block1 prev"></div>')	.button({text: false, icons: { primary: "ui-icon-seek-prev" }} ).css({"width": settings.buttonSize, "height": settings.buttonSize, "float": "left"})
-			, play: 	$('<div class="block1 play"></div>')	.button({text: false, icons: { primary: "ui-icon-play" }} )		.css({"width": settings.buttonSize, "height": settings.buttonSize, "float": "left"})
-			, pause: 	$('<div class="block1 pause"></div>')	.button({text: false, icons: { primary: "ui-icon-pause" }} )	.css({"width": settings.buttonSize, "height": settings.buttonSize, "float": "left"})
-			, stop: 	$('<div class="block1 stop"></div>')	.button({text: false, icons: { primary: "ui-icon-stop" }} )		.css({"width": settings.buttonSize, "height": settings.buttonSize, "float": "left"})
-			, next: 	$('<div class="block1 next"></div>')	.button({text: false, icons: { primary: "ui-icon-seek-next" }} ).css({"width": settings.buttonSize, "height": settings.buttonSize, "float": "left"})
-			, loop:		$('<div class="block3 loop"></div>')			.button({text: false, icons: { primary: "ui-icon-refresh" }} )//.css({"width": settings.buttonSize/2, "height": settings.buttonSize/2, "float": "left"})
-			, random:	$('<div class="block3 rnd"></div>')				.button({text: false, icons: { primary: "ui-icon-shuffle" }} )//.css({"width": settings.buttonSize/2, "height": settings.buttonSize/2, "float": "left"})
-			, playlist: 	$('<div class="block1 playlist"></div>').button({text: false, icons: { primary: "ui-icon-eject" }} ).css({"width": settings.buttonSize, "height": settings.buttonSize, "float": "left"})
-			, screen:	$('<canvas class="block1" height="15" width="99" ></canvas>')	.css({"border-radius": "2px", "border": 1, "clear": "both"})
-			, seekbar:	$('<div class="block1 seekbar"></div>')			.progressbar()	.css({"height":settings.buttonSize/3, "clear": "both"})
-			, volume:	$('<div class="block2 volume"></div>')			.slider({ orientation: "vertical", "value": settings.volume}).css({"font-size":"0.8em"})
+			drag:	 	$("<div style='height: 10; clear: both; margin-bottom:4' class='ui-widget-header ui-corner-all'></div>")
+			, prev: 	$('<div class="prev"></div>')	.button({text: false, icons: { primary: "ui-icon-seek-prev" }} ).css({"width": button_1, "height": button_1, "float": "left"})
+			, play: 	$('<div class="play"></div>')	.button({text: false, icons: { primary: "ui-icon-play" }} )	.css({"width": button_1, "height": button_1, "float": "left"})
+			, pause: 	$('<div class="pause"></div>')	.button({text: false, icons: { primary: "ui-icon-pause" }} )	.css({"width": button_1, "height": button_1, "float": "left"})
+			, stop: 	$('<div class="stop"></div>')	.button({text: false, icons: { primary: "ui-icon-stop" }} )	.css({"width": button_1, "height": button_1, "float": "left"})
+			, next: 	$('<div class="next"></div>')	.button({text: false, icons: { primary: "ui-icon-seek-next" }} ).css({"width": button_1, "height": button_1, "float": "left"})
+			, loop:		$('<div class="loop"></div>')	.button({text: false, icons: { primary: "ui-icon-refresh" }} ).css({"width": miniButtonSize, "height": miniButtonSize, "float": "left", "clear": "both"})
+			, shuffle:	$('<div class="shuffle"></div>').button({text: false, icons: { primary: "ui-icon-shuffle" }} ).css({"width": miniButtonSize, "height": miniButtonSize, "float": "left", "clear": "both"})
+			, playlist: 	$('<div class="playlist"></div>').button({text: false, icons: { primary: "ui-icon-eject" }} ).css({"width": button_1, "height": button_1, "font-size":"0.9em", "float":"left"})
+			, screen:	$('<canvas class="screen"></canvas>').css({"border-radius": "2px", "border": 1, "clear": "both", "height": button_3})
+			, seekbar:	$('<div class="seekbar"></div>').progressbar()	.css({"clear": "both", "height": button_3})
+			, volume:	$('<div class="volume"></div>')	.slider({ orientation: "vertical", "value": settings.volume}).css({"font-size":"0.8em", "height": button_1+(2*button_3)})
+			, audio:	$("<audio></audio>")			
+			, queue:	$("<ol class='queue ui-widget ui-widget-content ui-corner-all'></ol>").selectable().hide().css({"position":"absolute", "list-style-type":"none", "margin": 0, "padding": 7, "font-size": fontSize, "overflow-y":"scroll", "max-height":"200px"})
 		};
 		
 		// attach plugin events to the controls
@@ -70,13 +76,13 @@
 		  if ($this.hasClass(css.active)) $this.removeClass(css.active);
 		  else $this.addClass(css.active);
 		});
-		controls.random.on("click",		function() {
+		controls.shuffle.on("click",		function() {
 		  var $this = $(this);
 		  if ($this.hasClass(css.active)) $this.removeClass(css.active);
 		  else $this.addClass(css.active);
 		});
 		controls.playlist.on("click",	function() {
-			if (controls.queue.is(":visible")) controls.queue.fadeOut(/*function() { $(this).menu("destroy").hide(); }*/);
+			if (controls.queue.is(":visible")) controls.queue.fadeOut();
 			else controls.queue.fadeIn().position({ my: "left top", at: "right+5 top", of: container});
 		});
 		controls.seekbar.on("click", 	function(evt) {
@@ -90,39 +96,41 @@
 			p.volume = ui.value / 100;
 		});
 		
-		// push controls into the container		
-		block1.append(controls.prev, controls.play, controls.pause, controls.stop, controls.next, block3, controls.playlist, controls.screen, controls.seekbar);
-		block2.append(controls.volume);
-		block3.append(controls.random, controls.loop);
-		container.append(controls.audio, controls.queue, block1, block2);
+		// push controls
+		var tdcss = {"border":0, "margin":0, "padding":0};
+		settings.layout.find(".drag-container").css(tdcss)	.append(controls.drag);
+		settings.layout.find(".prev-container").css(tdcss)	.append(controls.prev);
+		settings.layout.find(".play-container").css(tdcss)	.append(controls.play);
+		settings.layout.find(".pause-container").css(tdcss)	.append(controls.pause);
+		settings.layout.find(".stop-container").css(tdcss)	.append(controls.stop);
+		settings.layout.find(".next-container").css(tdcss)	.append(controls.next);
+		settings.layout.find(".playlist-container").css(tdcss)	.append(controls.playlist);
+		settings.layout.find(".shuffleloop-container").css(tdcss).append(controls.shuffle, controls.loop);
+		settings.layout.find(".volume-container").css(tdcss)	.append(controls.volume);
+		settings.layout.find(".screen-container").css(tdcss)	.append(controls.screen);
+		settings.layout.find(".seekbar-container").css(tdcss)	.append(controls.seekbar);
+		container.append(settings.layout, controls.audio)
+			.after(controls.queue)
+			.draggable({handle: controls.drag})
+			.css({"font-size": 9, "padding": 8})
+			.addClass("ui-widget ui-widget-content ui-corner-all");
 		
-		// width must be calculated after the font-size
-		block3.height(settings.buttonSize).width(settings.buttonSize/2);
-		controls.random.width(miniButtonSize).height(miniButtonSize);
-		controls.loop.width(miniButtonSize).height(miniButtonSize);
-		
-		var fullWidth = (settings.buttonSize*7) + 6; // FIXED VAL
-		block1.width(fullWidth);
-		console.log(controls.play.outerWidth(true) + " " + (controls.play.outerWidth(true)/2) + " = " + fullWidth);
-
-		block2.width(controls.play.outerWidth(true) / 2); // half button
-		
-		controls.volume.css("height", block1.innerHeight() - utils.vborder(controls.volume));
-		container.css({"width": block1.outerWidth(true) + block2.outerWidth(true) + 10, "height": block1.outerHeight()}); // FIXED VAL
 		
 		// canvas
-		controls.screen.attr("width", block1.innerWidth());
-		controls.screen.attr("height", settings.buttonSize/3);
+		var w = controls.seekbar.width();
+		controls.screen.css("width", w)
+		controls.screen.attr("width", w);
+		controls.screen.attr("height", button_3);
 		var c = controls.screen[0];
 		var ctx = c.getContext('2d');
-		ctx.font = c.height + "px 'Varela Round'"; // Verdana
-		
-		if (settings.fontColor === null) settings.fontColor = $(".ui-widget-content").css("color");
-		console.log(settings.fontColor);
+		ctx.font = fontSize + "px 'Varela Round'"; // Verdana
+		fontColor = $(".ui-widget-content").css("color");
 		ctx.fillStyle = "rgba(0,0,0,0)";
 		ctx.fillRect(0,0,c.width,c.height);
 		
-		// little hack
+		// final size fixes
+		container.css({"width": settings.layout.outerWidth(true), "height": settings.layout.outerHeight(true)});
+		// selectable's little hack
 		/*var colorSelected = $("<div class='ui-state-highlight'></div>").css("background-color");//ui-state-selected ui-state-focus ui-state-highlight ui-state-active
 		var colorSelecting = $("<div class='ui-state-focus'></div>").css("background-color");
 		$("<style>").prop("type", "text/css").html(".ui-selecting { background: " + colorSelecting + "; }").appendTo("head");
@@ -153,11 +161,14 @@
 		// draw
 		ctx.clearRect(0,0,c.width(),c.height());
 		var size = c.height();
-		ctx.fillStyle = settings.fontColor;
+		ctx.fillStyle = fontColor;
 		ctx.fillText(text, offset, size);
 		
 		// loop
-		if (animationOn === true) window.requestAnimationFrame(tick); // setTimeout(tick,1000);
+		if (animationOn === true) {
+			if (typeof(window.requestAnimationFrame) == "undefined") setTimeout(tick, 20);
+			else window.requestAnimationFrame(tick);
+		}
 	}
 	
 	/* ::::::::::::::::::: */
@@ -173,9 +184,20 @@
 		, deselectAll: function(container) {
 			container.find(".ui-button").removeClass(css.active);
 		}
+		, resize: function(items, w, h) {
+			var size = { width:0, height:0 };
+			$(items).each(function(i,o) {
+				var $o = $(o);
+				if (w) $o.css("width", w);
+				if (h) $o.css("height", h);
+				size.width += $o.outerWidth(true);
+				size.height = Math.max($o.outerHeight(true), size.height);
+			});
+			return size;
+		}
 		, select: function(cmd) {
 			var isLoop = controls.loop.hasClass(css.active);
-			var isRnd = controls.random.hasClass(css.active);
+			var isRnd = controls.shuffle.hasClass(css.active);
 			var selected = controls.queue.children("li." + css.playing); // little hacky
 			var next = $();
 			var todo = controls.queue.children("li:not(.played)");
@@ -201,7 +223,7 @@
 				else if (cmd === "prev")
 					next = selected.prev("li");
 					
-				// now handle special cases (random, loop, ..)
+				// now handle special cases (shuffle, loop, ..)
 				if (selected.length == 0)
 					selected = controls.queue.children("li:first");
 					
@@ -270,13 +292,14 @@
 	
 	$.fn.jap = function(action, param) {
 		if (typeof(action) != "string") {
-			console.log("init!");
+			//console.log("init!");
+			dbg("init!");
 			init(action, this);
 		}
 		else 
 		{
-			if (typeof(param) == "undefined") console.log("%s: -", action);
-			else console.log("%s: %s", action, JSON.stringify(param));
+			if (typeof(param) == "undefined") dbg("%s: -", action); // console.log("%s: -", action);
+			else dbg("%s: %s", action, JSON.stringify(param)); // console.log("%s: %s", action, JSON.stringify(param));
 			
 			//
 			if (action === "enqueue") { // adds one element to the (html 'li') queue. Param: {src: "title.mp2", metadata: (tipically json)}
@@ -287,7 +310,7 @@
 				var li = $('<li style="font-weight: normal; border: 0px" >' + txt + '</li>"')
 				      .attr("src", param.src)
 				      .data(param.metadata)
-				      .on("keydown", function(evt) {console.log(event.which)});
+				      .on("keydown", function(evt) {dbg(event.which)});
 				/*
 				var doTheFix = false;
 				if (controls.queue.hasClass("ui-selectable"))
@@ -392,11 +415,30 @@
 }(jQuery));
 
 $.fn.jap.defaults = {
-	url: "127.0.0.1:80"
-	, buttonSize: 30
+	buttonSize: 30
 	, volume: 20
-	, fontColor: null // "#00FFC6"
 	, formatTitle: function(src, metadata) {
 		return src;
 	}
+	, layout: $('<table> \
+		<tr> \
+			<td class="drag-container" colspan="8" ></td> \
+		</tr> \
+		<tr> \
+			<td class="prev-container"></td> \
+			<td class="play-container"></td> \
+			<td class="pause-container"></td> \
+			<td class="stop-container"></td> \
+			<td class="next-container"></td> \
+			<td class="shuffleloop-container"></td> \
+			<td class="playlist-container"></td> \
+			<td class="volume-container" rowspan="3" ></td> \
+		</tr> \
+		<tr> \
+			<td class="screen-container" colspan="7" ></td> \
+		</tr> \
+		<tr> \
+			<td class="seekbar-container" colspan="7" ></td> \
+		</tr> \
+		</table>')
 }
